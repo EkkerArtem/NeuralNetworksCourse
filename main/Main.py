15e2fff7a6bf1313bdcd8166a90498c6c24044a7
ArtemEkker <ekkerartem@gmail.com>
2019-11-30 18:25:28 +0200

    Estimation of linear regression coefficients task added.

--- main/Main.py
+++ main/Main.py
@@ -28,6 +28,31 @@ import numpy as np
 # a = w.dot([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
 # print(a)
 
-f = urlopen("https://stepic.org/media/attachments/lesson/16462/boston_houses.csv")
-sbux = np.loadtxt(f, skiprows=1, delimiter=",", unpack=True)
-print(sbux.mean(axis=1))
+# f = urlopen("https://stepic.org/media/attachments/lesson/16462/boston_houses.csv")
+# sbux = np.loadtxt(f, skiprows=1, delimiter=",", unpack=True)
+# print(sbux.mean(axis=1))
+
+# arr = ((0, -0.5), (1, 0), (0, 0.5), (0, 1.5), (1, 2.5), (2,3))
+# def sqerr(a, b):
+#   return (a - b)**2
+# def abserr(a, b):
+#   return abs(a - b)
+# def errsum(err_fun):
+#   return sum(list(map(lambda pair: err_fun(pair[0], pair[1]), arr)))
+# print(str(max(errsum(abserr), errsum(sqerr))))
+
+
+
+# Оценка коэфициетов линейной регрессии для
+# ⎛⎝D  |  V
+#   10  | 60
+#    7  | 50
+#   12⎠ | 75
+# где D- тормозной путь автомобиля, V- скорость автомобил, результаты - b0 и b1 (b - бета с крышечкой)
+X = np.array([[1, 60], [1, 50], [1, 75]])
+y = np.array([[10], [7], [12]])
+step1 = X.T.dot(X)
+step2 = np.linalg.inv(step1)
+step3 = step2.dot(X.T)
+b = step3.dot(y)
+print(b)
