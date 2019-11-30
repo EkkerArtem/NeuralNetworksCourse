fb5f8290ee8d1005d3cb87faf03dc561b4bc337d
ArtemEkker <ekkerartem@gmail.com>
2019-11-30 17:10:49 +0200

    Initial commit

---
+++ main/Main.py
@@ -0,0 +1,29 @@
+import random
+
+import numpy as np
+
+# a = np.array([[1, 2, 3], [4, 5, 6]])
+# print(a)
+# print(a.shape)
+
+# print(np.eye(5, 3, k=-1))
+
+# w = np.array(random.sample(range(1000), 12))  # одномерный массив из 12 случайных чисел от 1 до 1000
+# w = w.reshape((2, 2, 3))  # превратим w в трёхмерную матрицу
+# print(w)
+
+# print(w.transpose(0, 2, 1))
+
+# ww = np.array(random.sample(range(1000), 12))  # одномерный массив из 12 случайных чисел от 1 до 1000
+# print(np.asmatrix(ww).T)
+
+# v = np.array([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
+# print(v)
+# print(v.mean(axis=0))  # вдоль столбцов
+# print(v.mean(axis=1))  # вдоль строк
+# print(v.mean(axis=None))  # вдоль всего массива
+
+# w = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+# a = w.dot([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+# print(a)
+
